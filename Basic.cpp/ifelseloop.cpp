e33ca809a069478f7be144dc2216543043333296
Shaurya0117 <shaurya7185@gmail.com>
2026-02-03 17:16:38 +0530

    comparison of 2 nums

---
+++ Basic.cpp/ifelseloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main( ){
+    int a,b;
+    cout<< "Enter 2 nums :";
+    cin>> a >> b;
+    if(a<=b){
+        cout<<"A is true :";
+    }
+    else{
+        cout<<"B is true :";
+    }
+    return 0;
+}
