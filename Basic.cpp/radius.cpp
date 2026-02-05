50186844c572ebd0a09b478e31127b30954f5ba5
Shaurya0117 <shaurya7185@gmail.com>
2026-02-05 14:22:20 +0530

    page

---
+++ Basic.cpp/radius.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r;
+    cin>>r;
+    floatcd cpp-programming
+     a = 3.142*r;
+    cout<<"Radius of num is : "<<a << endl;
+    return 0;
+    
+
+}
