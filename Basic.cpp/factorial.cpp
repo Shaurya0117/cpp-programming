a88942c5ec93d3b6819dbbbf1cdcb485d2f00ae1
Shaurya0117 <shaurya7185@gmail.com>
2026-02-07 23:42:20 +0530

    factorial

---
+++ Basic.cpp/factorial.cpp
@@ -0,0 +1,11 @@
+include <iostream>
+using namespace std;
+int main( ){
+    int a,;
+    cout<< "Enter  num :";
+    cin>> a;
+    for(int i =n/2; i>=1; i--){
+        if(n%i==0){
+            cout<<i<<endl;
+        }
+    }
