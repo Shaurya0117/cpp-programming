fcd773da49838aea4dca1aa7b18a70c016934bad
Shaurya0117 <shaurya7185@gmail.com>
2026-02-07 23:48:11 +0530

    1to100

---
+++ Basic.cpp/nto100.cpp
@@ -0,0 +1,10 @@
+include <iostream>
+using namespace std;
+int main( ){
+    int n;
+    cout<< "Enter  num :";
+    cin>>n;
+    for(int i =n; i<=100; i++){
+            cout<<i<<endl;
+        }
+    }
