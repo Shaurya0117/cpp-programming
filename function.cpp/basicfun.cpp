ef7cd09c45416064088aab1e96f4559572abaf12
Shaurya0117 <shaurya7185@gmail.com>
2026-02-18 16:31:17 +0530

    function basic

---
+++ function.cpp/basicfun.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void greet(){
+    cout<<"hey how are you";
+    
+}
+
+int main(){
+    greet();
+    cout<<endl<<"i am fine";
+}
---
+++ function.cpp/multiplefun.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void greet(){
+    cout<<"hey how are you";
+    return ;
+    
+}
+void india(){
+    cout<<"india is my country";
+    return ;
+}
+
+
+int main(){
+    greet();
+    cout<<endl<<"i am fine"<<endl;
+    india();
+    cout<<endl;
+    greet();
+    return 0;
+}
