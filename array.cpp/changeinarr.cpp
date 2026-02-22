8535b4b8998b77a8fb2337a67a2621a4c2729900
Shaurya0117 <shaurya7185@gmail.com>
2026-02-22 12:43:52 +0530

    array loop

---
+++ array.cpp/changeinarr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // 5 integer -> 1,2,3,4,5
+    int x[5] = {1,2,3,4,5};
+    int x[2] = {7};
+    cout<< x[2];
+    return 0;
+       
+}
---
+++ array.cpp/looparr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // 5 integer -> 1,2,3,4,5
+    int x[5] = {1,2,3,4,5};
+    for(int i=0; i<=4; i++){
+
+    
+    cout<< x[i] <<endl;
+    }
+    return 0;
+       
+}
