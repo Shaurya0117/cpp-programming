7b1d459771d500d2215b60bfa523bdbb619df158
Shaurya0117 <shaurya7185@gmail.com>
2026-02-22 12:59:40 +0530

    array size

---
+++ array.cpp/for-each.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // 5 integer -> 1,2,3,4,5
+    int x[5] = {1,2,3,4,5};
+    for(int num : x){
+
+    
+    cout<< num << "\n";
+    }
+    return 0;
+       
+}
---
+++ array.cpp/size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // 5 integer -> 1,2,3,4,5
+    int x[5] = {1,2,3,4,5};
+
+    
+    cout<< sizeof(x)/4;
+    
+    return 0;
+       
+}
