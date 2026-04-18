1b3ad1b7d8818de5c1871b1f620578d3312fc924
Shaurya0117 <shaurya7185@gmail.com>
2026-04-18 20:20:47 +0530

    array

---
+++ Array_Pointer/Pointer.cpp/adress.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int a=10;
+    int * ptr = &a;
+    cout<<ptr;
+    cout<< &a <<endl;
+    return 0;
+}
+
+
+// Pointer are special variable that store address of other variables
