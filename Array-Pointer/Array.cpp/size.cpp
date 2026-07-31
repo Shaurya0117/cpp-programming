9180e60ec7d82c18078f4e0c71b522f07ece3eda
Shaurya0117 <shaurya7185@gmail.com>
2026-07-31 20:03:36 +0530

    basic  commit

--- Array-Pointer/Array.cpp/size.cpp
+++ Array-Pointer/Array.cpp/size.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main(){
     // 5 integer -> 1,2,3,4,5
-    int x[5] = {1,2,3,4,5};
+    int x[] = {1,2,3,4,5};
 
     
     cout<< sizeof(x)/4;
+    
+    cout<<siz
     
     return 0;
        
