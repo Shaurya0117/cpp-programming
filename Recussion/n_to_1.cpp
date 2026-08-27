992e6d21811011c9365e28b4cd6a21bfb99c1890
Shaurya0117 <shaurya7185@gmail.com>
2026-08-27 09:36:53 +0530

    basic Recursion coden

--- Recussion/n_to_1.cpp
+++ Recussion/n_to_1.cpp
@@ -1,12 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void fun(int n){
-    if(n==1){
-        return 1;
-
+void fun(int n)
+{
+    if(n == 0)
+    {
+        return ; 
     }
-    cout<<n;
-    fun(n-1);
-    
+
+    cout << n << " ";
+    fun(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter num: ";
+    cin >> n;
+
+    cout << "Numbers from n to 1 are: ";
+    fun(n);
+
+    return 0;
 }
